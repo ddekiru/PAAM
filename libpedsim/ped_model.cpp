d4846c32553e8911210815afb1970e6fb6926838
cdamian <cadardamiann@gmail.com>
2023-05-04 10:22:25 +0200

    lab3 prepared

--- libpedsim/ped_model.cpp
+++ libpedsim/ped_model.cpp
@@ -51,8 +51,6 @@ void Ped::Model::tick()
   //   x->setY(x->getDesiredY());
   // }
 
-
-
 //########### OpenMP
 // {
 //   // Compute the next desired position for each agent
@@ -66,17 +64,20 @@ void Ped::Model::tick()
 
 //########## threads
 // {
-//   std::vector<std::thread> threads;
-  
-//   for (int i = 0; i < agents.size(); i++) {
-//     threads.emplace_back([this, i]() {
-//       agents[i]->computeNextDesiredPosition();
-//       agents[i]->setX(agents[i]->getDesiredX());
-//       agents[i]->setY(agents[i]->getDesiredY());
+//   thread* threads = new thread[4];
+//   for (int tid = 0; tid < 4; ++tid) {
+//     threads[tid] = thread([this, tid]() {
+//       int from = tid * agents.size() / 4;
+//       int to = (tid + 1) * agents.size() / 4;
+//       for (int i = from; i < to; ++i) {
+//         agents[i]->computeNextDesiredPosition();
+//         agents[i]->setX(agents[i]->getDesiredX());
+//         agents[i]->setY(agents[i]->getDesiredY());
+//       }
 //     });
 //   }
-//   for (auto& thread : threads) {
-//     thread.join();
+//   for (int tid = 0; tid < 4; tid++) {
+//     threads[tid].join();
 //   }
 // }
 
